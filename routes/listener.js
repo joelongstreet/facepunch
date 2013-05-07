74c32261f99a0b5121d5b91e3e69243a722062eb
joelongstreet <joelongstreet@gmail.com>
2013-05-07 18:56:51 -0500

    code generation for listener view

--- routes/listener.js
+++ routes/listener.js
@@ -1,3 +1,28 @@
 exports.index = function(req, res){
-    res.render('listener');
+    var generateCode = function(next){
+        var code = randomString(5);
+        if(connectedCodes.indexOf(code) == -1){
+            next(code);
+        } else {
+            generateCode(next);
+        }
+    };
+
+    generateCode(function(code){
+        res.render('listener', { code : code });
+    });
 };
+
+var randomString = function(length){
+    var charSet     = 'abcdefghjkmnpqrstuvwxyz23456789';
+    var rando       = '';
+    var iterator    = 0;
+
+    while (iterator < length){
+        var randPos = Math.floor(Math.random() * charSet.length);
+        rando += charSet.substring(randPos, randPos + 1);
+        iterator++;
+    }
+
+    return rando;
+}
