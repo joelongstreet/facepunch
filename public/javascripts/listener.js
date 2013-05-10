51d12696310850fb0a56103a98ca1d64da61b164
joelongstreet <joelongstreet@gmail.com>
2013-05-10 01:02:40 -0500

    adding share buttons, Fixes #8

--- public/javascripts/listener.js
+++ public/javascripts/listener.js
@@ -1,5 +1,12 @@
 var socket = io.connect('/');
 var count  = 1;
+var shares = [
+    'I just facepunched someone *** times... it could have been you',
+    'My fist hurts after the beating I just gave. *** punches',
+    'Just delivered *** knuckle sandwiches to someone\'s face, was it you?',
+    'MMA ain\'t got nothin\' on me. *** punches and counting',
+    'Face, meet fist. *** punches and I\'m not slowin\' down',
+];
 
 socket.on('punch', function(data){
     // We're expecting a number roughly between 10 and 20ish
@@ -46,6 +53,20 @@ $(function(){
         fileReader.readAsDataURL(e.dataTransfer.files[0])
     }, false);
 
+    // Share on Twitter
+    $('#twitter_share').click(function(e){
+        var tweet_earl = 'https://twitter.com/intent/tweet?hashtags=facepunch&original_referer=http://facepunch.jit.su&text=' + generateShare() + '&url=http://facepunch.jit.su';
+        popUpWindow(tweet_earl, 'Twitter');
+        return false
+    });
+
+    // Share on Facebook
+    $('#facebook_share').click(function(){
+        var fb_earl = 'https://www.facebook.com/sharer/sharer.php?s=100&p[title]=facepunch&p[summary]=' + generateShare() + '&p[url]=http://facepunch.jit.su'
+        popUpWindow(fb_earl, 'Facebook');
+        return false
+    });
+
     // Pair the broadcast client with the server
     // this timeout has got to go, i would think there should be a ready event?
     setTimeout(function(){
@@ -61,3 +82,18 @@ $(function(){
         });
     }, 500);
 });
+
+
+var popUpWindow = function(earl, title){
+    var left = (window.screen.width / 2) - 260;
+    window.open(earl, title, 'height=270, width=500, scrollbars=no, screenX=' + left + ', screenY=100');
+};
+
+
+var generateShare = function(){
+    var rando = Math.floor(Math.random()*shares.length)
+    var item  = shares[rando];
+    var text  = item.replace('***', count);
+
+    return text;
+};
